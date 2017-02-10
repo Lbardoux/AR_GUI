5ffbbf89f18b2bc4c20a676968d3e0b6b358ebff
Zelnes <mehdi.ghesh@etu.univ-lyon1.fr>
2017-02-10 11:11:46 +0100

    Ajout de l'include <opencv2/imgproc/imgproc.hpp>

--- includes/Cv_core.hpp
+++ includes/Cv_core.hpp
@@ -14,9 +14,10 @@
 // ############################################################################
 // Liste des includes
 
-#include <opencv2/opencv.hpp>
-#include <opencv/cv.h>
+// #include <opencv2/opencv.hpp>
+// #include <opencv/cv.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 // ############################################################################
 // Définition des maccros
