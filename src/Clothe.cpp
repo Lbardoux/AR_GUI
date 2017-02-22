936313bc728ce65ad94ce5973cf2260589b6d09b
CharlesSultan <c.sultan@hotmail.fr>
2017-02-23 00:17:53 +0100

    Je me suis battu contre cette merde mais il reste un bug que je n'arrive pas à comprendre ...

--- src/Clothe.cpp
+++ src/Clothe.cpp
@@ -19,6 +19,39 @@ float dot(Vector v1, Vector v2)
     return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
 }
 
+Transform rotationMatrix(const float angle, Vector& axis)
+{
+    Vector a= axis.normalize();
+    float s= sinf(angle);
+    float c= cosf(angle);
+
+    return Transform(
+        a.x() * a.x() + (1 - a.x() * a.x() ) * c,
+        a.x() * a.y() * (1 - c ) - a.z() * s,
+        a.x() * a.z() * (1 - c ) + a.y() * s,
+        0,
+
+        a.x() * a.y() * (1 - c ) + a.z() * s,
+        a.y() * a.y() + (1 - a.y() * a.y() ) * c,
+        a.y() * a.z() * (1 - c ) - a.x() * s,
+        0,
+
+        a.x() * a.z() * (1 - c ) - a.y() * s,
+        a.y() * a.z() * (1 - c ) + a.x() * s,
+        a.z() * a.z() + (1 - a.z() * a.z() ) * c,
+        0,
+
+        0, 0, 0, 1);
+}
+
+Vector scale_product(Vector & u, Vector & v)
+{
+    return Vector(
+        (u.y() * v.z()) - (u.z() * v.y()),
+        (u.z() * v.x()) - (u.x() * v.z()),
+        (u.x() * v.y()) - (u.y() * v.x()));
+}
+
 void Clothe::draw(const Point & position1, const Point & position2, const Transform & view, 
     const Transform & projection) const
 {
@@ -38,13 +71,30 @@ void Clothe::draw(const Point & position1, const Point & position2, const Transf
     if(position1.y() > position2.y()) angle_z = -angle_z;
     Matrix rotation_z_matrix = rotationZMatrix(angle_z);
 
-    //La rotation selon x
-    Vector p1_p2_y = Vector(p1_p2.x(), 0, p1_p2.z()).normalize(); 
-    float cos_angle_y = dot(p1_p2_y, Vector(1, 0, 0));
-    float angle_y = acos(cos_angle_y);
-    if(position1.z() > position2.z()) angle_y = -angle_y;
-    Matrix rotation_y_matrix = rotationYMatrix(angle_y);
+    
+    //Vector normal = Vector(cos(angle_z + M_PI/2), sin(angle_z + M_PI/2), 0);
+    Vector normal = Vector(cos(0 + M_PI/2), sin(0 + M_PI/2), 0);//OK
+    //std::cout<<"normal 				: "<<normal.x()<<" "<<normal.y()<<" "<<normal.z()<<" "<<std::endl;
+    Vector p1_p2_proj = ( p1_p2 + (normal * (-dot(p1_p2, normal))) ).normalize();//OK
+    /*std::cout<<"p1_p2 				: "<<p1_p2.x()<<" "<<p1_p2.y()<<" "<<p1_p2.z()<<" "<<std::endl;
+    std::cout<<"dot(p1_p2, normal)	: "<<dot(p1_p2, normal)<<std::endl;
+    std::cout<<"p1_p2_proj 			: "<<p1_p2_proj.x()<<" "<<p1_p2_proj.y()<<" "<<p1_p2_proj.z()<<" "<<std::endl;
+    std::cout<<std::endl;*/
+    					//(projeté du point p1_p2 sur le plan de normale n) 
+    				 	//(http://forum.mathematex.net/exercices-problemes-superieur-f18/projection-d-un-point-sur-un-plan-t923.html)
+    Vector z = Vector(0, 0, 1);
+    Vector osef = scale_product(normal, z);
+    float cos_angle_proj = dot(p1_p2_proj, osef);
+    std::cout<<"p1_p2_proj 			: "<<p1_p2_proj.x()<<" "<<p1_p2_proj.y()<<" "<<p1_p2_proj.z()<<" "<<std::endl;
+    std::cout<<"cos_angle_proj		: "<<cos_angle_proj<<std::endl;
+    float angle_proj = acos(cos_angle_proj);
+    std::cout<<"angle_proj avant	: "<<angle_proj * 180 / M_PI<<std::endl;
+    //if(dot(p1_p2, z) < 0) angle_proj = -angle_proj;
+    //angle_proj -= M_PI;
+    std::cout<<"angle_proj apres	: "<<angle_proj * 180 / M_PI<<std::endl;
+    Matrix rotation_proj_matrix = rotationYMatrix(angle_proj);//rotationMatrix(angle_proj, normal);
+    
 
-    Transform model = translation_matrix * rotation_z_matrix * scale_matrix;
+    Transform model = translation_matrix * /*rotation_z_matrix */ rotation_proj_matrix * scale_matrix;
     m_mesh.draw(model, view, projection);
 }
